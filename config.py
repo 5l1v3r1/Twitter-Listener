957270a18149fe0a541e8212cf1c5117875ee660
Landon Newberry <ltnewberry@crimson.ua.edu>
2017-09-24 19:22:13 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,4 @@
-#--------------------------------------------------------------------------------
-# These tokens are needed for user authentication.
-# Credentials can be generates via Twitter's Application Management:
-#	https://apps.twitter.com/app/new
-#--------------------------------------------------------------------------------
-
-consumer_key = "YpdOww7gY66jKCa8cJlmTiIgE"
-consumer_secret = "oJQdLLx7pxy2YJD3sWfEqNU7JqfEil75vD2v8Y9efa694wboGU"
-access_key = "908339776679542784-uaSMEkjC4KJgnjc7Db2mIUyIlPZnJJF"
-access_secret = "NG1AoeNrXvjTBEDHdTilnXfasCAbkJKwAR0zEAJhIg7dT"
+consumer_key = "xxxxxxxxxxxxxxxxxxx"
+consumer_secret = "xxxxxx"
+access_key = "xxxxxxxxx"
+access_secret = "xxxxxxxxxx"
